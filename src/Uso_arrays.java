99f36f0400755d65397bc5af31f271fcb42bac4b
Julio Gabriel Milano <gablio@live.com>
2021-07-16 01:37:30 -0300

    Videos visto de arrays falta el numero 25

---
+++ src/Uso_arrays.java
@@ -0,0 +1,26 @@
+
+public class Uso_arrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int [] mi_matriz = new int [5];
+		
+		mi_matriz[0]=5;
+		mi_matriz[1]=38;
+		mi_matriz[2]=15;
+		mi_matriz[3]=92;
+		mi_matriz[4]=78;
+		
+
+		System.out.println(mi_matriz[0]);
+		
+		for ( int i=0; i<5; i++) {
+		
+		System.out.println(mi_matriz[i]);
+		
+		
+		
+}
+}
+}
---
+++ src/Uso_arrays2.java
@@ -0,0 +1,52 @@
+
+public class Uso_arrays2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//String [] paises = new paises[8];
+		
+	String [] paises = {" España", " Mexico"," Colombia"," Peru", " Chile"," Argentina"," Ecuador"," Venezuela"};
+		
+		//System.out.print(paises[4]);
+		
+	/*for (int i=0; i<paises.length; i++) {	
+		
+		System.out.println("Pais"+(i+1) +"  "+ paises[i]);
+	}
+	*/
+	
+	for (String elemento:paises) {
+		
+		
+		System.out.println(elemento);
+	}
+	
+	
+	}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+
